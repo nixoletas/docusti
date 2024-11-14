6ac8d661a6e262f807ec6efef8aeeb953f47d7da
Nicholas_Miyasato <66659340+nixoletas@users.noreply.github.com>
2024-11-14 19:46:21 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,7 +126,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Índice de aaaa`,
+        copyright: `Índice de bbbbb`,
       },
       prism: {
         theme: prismThemes.github,
